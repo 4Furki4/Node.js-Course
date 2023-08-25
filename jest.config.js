87637dacabfe8adc69bc1e214855c1a77dd21d9f
Furkan Cengiz <muhammedcengiz1@gmail.com>
2023-08-25 23:20:28 +0300

    add: unit testing with jest

---
+++ jest.config.js
@@ -0,0 +1,6 @@
+//npx ts-jest config:init
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+};
---
+++ src/handlers/tests/user.test.ts
@@ -0,0 +1,8 @@
+describe('user handler', () => {
+    it('should do something when something happens', () => {
+        expect(1).toEqual(1)
+    })
+    it('should do something when something happens', () => {
+        expect(1).toEqual(2)
+    })
+})
